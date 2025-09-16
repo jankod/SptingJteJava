3b08976903ea3c77a21cc6a00e20a46a03bb8113
jdiminic <jdiminic@gmail.com>
2025-09-16 09:57:49 +0200

    add data tables

--- src/main/java/hr/ja/st/config/DataTablesConfiguration.java
+++ src/main/java/hr/ja/st/config/DataTablesConfiguration.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFa
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
-public class DataTablesConfiguration {}
+@EnableJpaRepositories(
+        basePackages = "hr.ja.st",
+        repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class
+)
+public class DataTablesConfiguration {
+}
